2638f560f7668341f29f26dc70e2dd67c45f12a2
VenuGopal-240 <sanaboinavenugopal@gmail.com>
2025-11-18 11:48:29 +0530

    git first commit

---
+++ client/src/Table/CurrentTask.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import { Table } from "./Table";
+import CancelIcon from "@mui/icons-material/Cancel";
+const CurrentTask = () => {
+    const columns = [
+        { id: "name", label: "Name" },
+        { id: "location", label: "Location" },
+        { id: "industry", label: "Industry" },
+        { id: "foundedYear", label: "FoundedYear" },
+        { id: "employees", label: "Employees" },
+        // { id: "revenue", label: "Revenue" },
+    ];
+
+    const [data, setData] = useState([])
+    useEffect(() => {
+        fetch("http://localhost:3000/companies").then((res) => res.json()).then((data) => setData(data?.data))
+    }, []);
+
+    return (
+        <>
+            <div
+                style={{
+                    width: "1100px",
+                    margin: "20px auto",
+
+                }}
+            >
+                <h2>Frontend Developer Assessment– Frontlines Media
+                </h2>
+                <Table
+                    columns={columns}
+                    data={data}
+                    searchble
+                    sortable
+                    noDataMsg={{
+                        name: "No Data Found",
+                        Icon: <CancelIcon />,
+                    }}
+                />
+
+            </div>
+        </>
+    );
+};
+export default CurrentTask;
